06d5487e74fac939a29d9b0d7527fa8130e5ffaf
Tinnon <etinnon@una.edu>
2023-10-05 12:52:08 -0500

    final product w out html stuff

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser")
 const { ObjectId } = require('mongodb')
 const port = (process.env.PORT || 5500)
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -36,13 +37,9 @@ async function run() {
 async function cxnDB(){
 
   try{
-    client.connect; 
-    const collection = client.db("emilys-db-for-papa").collection("dev-profiles");
-    // const collection = client.db("papa").collection("dev-profiles");
-    const result = await collection.find().toArray();
-    //const result = await collection.findOne(); 
-    console.log("cxnDB result: ", result);
-    return result; 
+    await client.connect().then(
+     client.db("emilys-db-for-papa").collection("dev-profiles"));
+    // const collection = client.db("papa").collection("dev-profiles");     
   }
   catch(e){
       console.log(e)
@@ -53,15 +50,94 @@ async function cxnDB(){
 }
 
 app.get('/', async (req, res) => {
-
-  let result = await cxnDB().catch(console.error); 
-
-  // console.log("get/: ", result);
-  res.send("just a test " + result[0].name)
- // res.render('index', {  peopleData : result })
+  client.connect;
+  let mongoResult = await client.db("emilys-db-for-papa").collection("dev-profiles").find().toArray();
+  console.log(mongoResult);
+  res.render('index', {  profileData : mongoResult })
 })
 
-let myVaraibleServer = 'soft coded server data';
+app.post('/updateProfile', async (req, res) => {
+
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
+    
+    client.connect; 
+    const collection = client.db("emilys-db-for-papa").collection("dev-profiles");
+  
+    // put it into mongo
+    let result = await collection.findOneAndUpdate( 
+      { _id: new ObjectId( req.body.devId ) },
+      {$set: {name: req.body.devName }})
+      .then(result => {
+        console.log(result); 
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+     
+   
+  }
+  finally{
+    //client.close()
+  }
+})
+
+app.post('/insertProfile', async (req, res) => {
+
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
+    
+    client.connect; 
+    const collection = client.db("emilys-db-for-papa").collection("dev-profiles");
+  
+    // put it into mongo
+    let result = await collection.insertOne( 
+      { name: req.body.newDevName })
+      .then(result => {
+        console.log(result); 
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+     
+   
+  }
+  finally{
+    //client.close()
+  }
+})
+
+app.post('/deleteProfile', async (req, res) => {
+
+  try {
+    //get the new dev name
+    console.log("body: ", req.body)
+    console.log("user Name: ", req.body.devName)
+    
+    client.connect; 
+    const collection = client.db("emilys-db-for-papa").collection("dev-profiles");
+  
+    // put it into mongo
+    let result = await collection.findOneAndDelete( 
+      { _id: new ObjectId( req.body.devId) })
+      .then(result => {
+        console.log(result); 
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
+     
+   
+  }
+  finally{
+    //client.close()
+  }
+})
+
+
+let myVariableServer = 'soft coded server data';
+
 
 app.get('/view', function (req, res) {
   res.render('index', {'myVariableClient': myVaraibleServer});
@@ -69,7 +145,7 @@ app.get('/view', function (req, res) {
 
 app.post('/postClientData', function (req, res) {
   
-  console.log("body:",req.body)
+  console.log("body:",req.body);
   console.log("user Name:", req.body.userName);
   res.render('index', {'myVariableClient': req.body.userName});
 })
